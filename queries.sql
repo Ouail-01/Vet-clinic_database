95cb9df1e2b33a6293b480aa292c54a5300cf176
Ouail-01 <ouaillaarabi@gmail.com>
2023-06-06 22:26:35 +0100

    Add queries

--- queries.sql
+++ queries.sql
@@ -65,3 +65,28 @@ SELECT * FROM animals;
 UPDATE animals SET weight_kg = weight_kg * -1 where weight_kg < 0;
 SELECT * FROM animals;
 COMMIT;
+
+/* How many animals are there? */
+
+SELECT COUNT(*) FROM animals;
+
+/* How many animals have never tried to escape? */
+
+SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
+
+/* What is the average weight of animals? */
+
+SELECT AVG(weight_kg) FROM animals;
+
+/* Who escapes the most, neutered or not neutered animals? */
+
+SELECT neutered, SUM(escape_attempts) FROM animals GROUP BY neutered;
+
+/* What is the minimum and maximum weight of each type of animal? */
+
+SELECT species, min(weight_kg) AS min_weight, max(weight_kg) AS max_weight FROM animals GROUP BY species;
+
+/* What is the average number of escape attempts per animal type of those born between 1990 and 2000? */
+
+SELECT species, AVG(escape_attempts) AS average_escape FROM animals 
+WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species;
