008e41a620b1c7102e509f1b5153b4c325cb3185
Ouail-01 <ouaillaarabi@gmail.com>
2023-06-06 01:50:02 +0100

    Update the queries

--- queries.sql
+++ queries.sql
@@ -4,7 +4,7 @@ SELECT * FROM animals WHERE name like '%mon';
 
 /* List the name of all animals born between 2016 and 2019. */
 
-SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-10';
+SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
 
 /* List the name of all animals that are neutered and have less than 3 escape attempts. */
 
